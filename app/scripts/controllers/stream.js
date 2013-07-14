6f604057797c0d3a747464a92e4a07c8818ed4e5
Espen Hovlandsdal <espen@hovlandsdal.com>
2013-07-14 18:15:02 +0200

    Fixed bug where the twitter stream would only work once.

--- app/scripts/controllers/stream.js
+++ app/scripts/controllers/stream.js
@@ -1,29 +1,30 @@
-/* global angular */
+/* global angular, _ */
 (function() {
     'use strict';
 
     var app = angular.module('zc');
     app.controller('StreamCtrl', [function() {
-
         // Window dimensions?
         var height = window.innerHeight - 45
           , width  = window.innerWidth;
 
         (function(doc, tag, id) {
             var a = doc.getElementsByClassName('twitter-timeline')[0]
-              , fjs  = doc.getElementsByTagName(tag)[0]
+              , fjs  = _.last(doc.getElementsByTagName(tag))
               , prot = /^http:/.test(doc.location) ? 'http' : 'https'
-              , js;
+              , js   = doc.getElementById(id);
 
             a.setAttribute('height', height);
             a.setAttribute('width',   width);
 
-            if (!doc.getElementById(id)) {
-                js = doc.createElement(tag);
-                js.id = id;
-                js.src = prot + '://platform.twitter.com/widgets.js';
-                fjs.parentNode.insertBefore(js, fjs);
+            if (js) {
+                js.parentNode.removeChild(js);
             }
+
+            js = doc.createElement(tag);
+            js.id = id;
+            js.src = prot + '://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js, fjs);
         })(document, 'script', 'twitter-wjs');
 
     }]);
