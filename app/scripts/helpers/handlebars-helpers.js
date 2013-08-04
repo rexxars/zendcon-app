e16fa98060be4fcd6bbf00d262fdd705e524520a
Espen Hovlandsdal <espen@hovlandsdal.com>
2013-08-04 22:38:57 +0200

    Print-friendlyness increased. Fixed a couple of issues with the meta section in Firefox.

--- app/scripts/helpers/handlebars-helpers.js
+++ app/scripts/helpers/handlebars-helpers.js
@@ -6,7 +6,7 @@ define(['underscore', 'handlebars'], function(_, Handlebars) {
             return new Handlebars.SafeString(text);
         }
 
-        text = Handlebars.Utils.escapeExpression(text);
+        text = Handlebars.Utils.escapeExpression(text.replace(/^\s+|\s+$/g, ''));
         var nl2br = (text + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + '<br>' + '$2');
         return new Handlebars.SafeString(nl2br);
     });
