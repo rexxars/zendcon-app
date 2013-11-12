fa0a3f4cfa8935b8027b56a9da3a870e6dca2d90
Espen Hovlandsdal <espen@hovlandsdal.com>
2013-11-12 22:44:43 +0100

    Added room numbers for monday sessions

--- backend/config/zceu-room-numbers.php
+++ backend/config/zceu-room-numbers.php
@@ -1,53 +1,65 @@
 <?php
 return array(
+    // Monday
+    9279  => 'Miles Davis AB',
+    10011 => 'Miles Davis C',
+    9282  => 'Louis Armstrong CD',
+    9280  => 'Ella Fitzgerald AB',
+    9281  => 'Louis Armstrong AB',
+
+    10012 => 'Miles Davis C',
+    9286  => 'Louis Armstrong AB',
+    9283  => 'Ella Fitzgerald AB',
+    9284  => 'Miles Davis AB',
+
     // Tuesday
-    9913 => 'Seine AB',
+    9913  => 'Seine AB',
 
-    9917 => 'Pont des Arts AB',
-    9258 => 'Seine AB',
-    9259 => 'Auditorium',
+    9917  => 'Pont des Arts AB',
+    9258  => 'Seine AB',
+    9259  => 'Auditorium',
 
-    9918 => 'Pont des Arts AB',
-    9260 => 'Seine AB',
-    9261 => 'Auditorium',
+    9918  => 'Pont des Arts AB',
+    9260  => 'Seine AB',
+    9261  => 'Auditorium',
 
-    9914 => 'Seine AB',
+    9914  => 'Seine AB',
 
-    9919 => 'Pont des Arts AB',
-    9263 => 'Seine AB',
-    9262 => 'Auditorium',
+    9919  => 'Pont des Arts AB',
+    9263  => 'Seine AB',
+    9262  => 'Auditorium',
 
-    9920 => 'Pont des Arts AB',
-    9264 => 'Seine AB',
-    9265 => 'Auditorium',
+    9920  => 'Pont des Arts AB',
+    9264  => 'Seine AB',
+    9265  => 'Auditorium',
 
-    9922 => 'Pont des Arts AB',
-    9266 => 'Seine AB',
-    9921 => 'Auditorium',
+    9922  => 'Pont des Arts AB',
+    9266  => 'Seine AB',
+    9921  => 'Auditorium',
 
     // Wednesday
-    9915 => 'Seine AB',
+    9915  => 'Seine AB',
 
-    9922 => 'Pont des Arts AB',
-    9271 => 'Seine AB',
-    9923 => 'Seince C',
+    9922  => 'Pont des Arts AB',
+    9271  => 'Seine AB',
+    9923  => 'Seince C',
 
-    9268 => 'Pont des Arts AB',
-    9273 => 'Seine AB',
-    9276 => 'Auditorium',
-    9272 => 'Seine C',
+    9268  => 'Pont des Arts AB',
+    9273  => 'Seine AB',
+    9276  => 'Auditorium',
+    9272  => 'Seine C',
 
-    9925 => 'Pont des Arts AB',
-    9270 => 'Seine AB',
-    9274 => 'Seine C',
+    9925  => 'Pont des Arts AB',
+    9270  => 'Seine AB',
+    9274  => 'Seine C',
 
-    9269 => 'Pont des Arts AB',
-    9275 => 'Seine AB',
-    9927 => 'Auditorium',
-    9926 => 'Seine C',
+    9269  => 'Pont des Arts AB',
+    9275  => 'Seine AB',
+    9927  => 'Auditorium',
+    9926  => 'Seine C',
 
-    9277 => 'Pont des Arts AB',
-    9278 => 'Seine C',
+    9277  => 'Pont des Arts AB',
+    9278  => 'Seine C',
 
-    9916 => 'Seine AB',
+    9916  => 'Seine AB',
 );
